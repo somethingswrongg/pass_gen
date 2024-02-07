cc2f9d497bf8519798d92b69407475f99c8041f5
John <vss0987@yandex.ru>
2024-02-07 14:14:18 +0300

    added models Profile, Passwords, UsersPasswords

--- generator/models.py
+++ generator/models.py
@@ -1,2 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class Profile(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(blank=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+
+
+class Passwords(models.Model):
+    password = models.CharField(max_length=12)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UsersPasswords(models.Model):
+    name_template = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    password_template = models.ForeignKey(Passwords, on_delete=models.CASCADE)
